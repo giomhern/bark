771973e80c3105486f1bcc4e30b9e4826e56d0e7
giovannimhern <giovannimhern@uchicago.edu>
2024-01-19 10:29:44 -0600

    Used app router to refresh when user logs out and restrict data access to only authenticated users

--- app/auth-button.tsx
+++ app/auth-button.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
 
 export default function AuthButton() {
   const supabase = createClientComponentClient({
     supabaseKey: process.env.NEXT_PUBLIC_ANON_KEY,
     supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
   });
+
+  const router = useRouter()
 
   const handleSignIn = async () => {
     await supabase.auth.signInWithOAuth({
@@ -18,6 +21,7 @@ export default function AuthButton() {
   };
   const handleSignOut = async () => {
     await supabase.auth.signOut();
+    router.refresh();
   };
   return (
     <div className="flex gap-10">
--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest){
     const res = NextResponse.next();
-    const supabase = createMiddlewareClient({req, res})
+    const supabase = createMiddlewareClient({req, res}, {supabaseKey: process.env.NEXT_PUBLIC_ANON_KEY, supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL})
     await supabase.auth.getSession();
     return res;
 }
