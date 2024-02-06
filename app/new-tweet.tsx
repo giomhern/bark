d66a14eb1743d46c93c7d82ea99c6e1ce858a31e
giovannimhern <giovannimhern@uchicago.edu>
2024-02-05 22:52:01 -0600

    Hope this works

--- app/new-tweet.tsx
+++ app/new-tweet.tsx
@@ -1,44 +1,40 @@
-"use client"
-import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
+"use client";
+import {
+  createClientComponentClient,
+  createServerActionClient,
+} from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import Image from "next/image";
 
 import type { User } from "@supabase/auth-helpers-nextjs";
 import { useState } from "react";
-
-export const dynamic = "force-dynamic";
+import { Tweet } from "./global";
 
 export default function NewTweet({ user }: { user: User }) {
+  const supabase = createClientComponentClient<Database>({
+    supabaseKey: process.env.NEXT_PUBLIC_ANON_KEY,
+    supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
+  });
   const [newTweet, setNewTweet] = useState("");
-  const addTweet = async (e: any) => {
-    e.preventDefault();
+
+  async function addTweet({ newTweet }: { newTweet: string }) {
     try {
-      const response = await fetch(
-        `${location.origin}/api/addTweet`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ tweet: newTweet, userId: user.id }),
-        }
-      );
-      console.log(response)
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      setNewTweet("");
-    } catch (error) {
-      console.error("Failed to submit tweet", error);
+      const { error } = await supabase
+        .from("tweets")
+        .insert({ tweet: newTweet, user_id: user.id });
+      if (Error) throw Error;
+      alert("tweet submitted!");
+    } catch {
+        alert("error submitting data!")
     }
-  };
+  }
 
   const handleTweetChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setNewTweet(e.target.value);
   };
 
   return (
-    <form className="border border-gray-800 border-t-0" onSubmit={addTweet} method="post">
+    <form className="border border-gray-800 border-t-0">
       <div className="flex py-8 px-4">
         <div className="h-12 w-12">
           <Image
@@ -56,6 +52,7 @@ export default function NewTweet({ user }: { user: User }) {
           onChange={handleTweetChange}
           value={newTweet}
         />
+        <button onClick={() => addTweet}>Submit</button>
       </div>
     </form>
   );
