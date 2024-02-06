8c1749940e2b3b72fb04380b22d98c3c8d07311e
giovannimhern <giovannimhern@uchicago.edu>
2024-02-05 23:08:48 -0600

    Added router refresh and styled the button a slight bit better

--- app/new-tweet.tsx
+++ app/new-tweet.tsx
@@ -9,26 +9,28 @@ import Image from "next/image";
 import type { User } from "@supabase/auth-helpers-nextjs";
 import { useState } from "react";
 import { Tweet } from "./global";
+import { useRouter } from "next/navigation";
 
 export default function NewTweet({ user }: { user: User }) {
   const supabase = createClientComponentClient<Database>({
     supabaseKey: process.env.NEXT_PUBLIC_ANON_KEY,
     supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
   });
+  const router = useRouter();
   const [newTweet, setNewTweet] = useState("");
-
+  const handleFormSubmit = (event: { preventDefault: () => void; }) => {
+    event.preventDefault(); // Prevent the form from submitting on Enter key or form action
+  };
   async function addTweet({ newTweet }: { newTweet: string }) {
     try {
       const { error } = await supabase
         .from("tweets")
         .insert({ tweet: newTweet, user_id: user.id });
-        console.log(newTweet);
-        console.log(user.id);
-        console.log(error)
-        
-      if (error) throw error;
+      console.log(newTweet);
+      console.log(user.id);
 
-      alert("tweet submitted!");
+      if (error) throw error;
+      router.refresh()
     } catch {
       alert("error submitting data!");
     }
@@ -39,7 +41,7 @@ export default function NewTweet({ user }: { user: User }) {
   };
 
   return (
-    <form className="border border-gray-800 border-t-0">
+    <form className="border border-gray-800 border-t-0" onSubmit={handleFormSubmit}>
       <div className="flex py-8 px-4">
         <div className="h-12 w-12">
           <Image
@@ -57,7 +59,12 @@ export default function NewTweet({ user }: { user: User }) {
           onChange={(e) => setNewTweet(e.target.value)}
           value={newTweet}
         />
-        <button className="ml-2 border border-white bg-white px-5 py-3" onClick={() => addTweet({ newTweet })}>Submit</button>
+        <button
+          className="ml-2 border border-white bg-white px-5 py-2"
+          onClick={() => addTweet({ newTweet })}
+        >
+          Submit
+        </button>
       </div>
     </form>
   );
