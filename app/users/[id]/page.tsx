261d29a0d9c72fe1813ebbda427e0cb9e8eda361
giovannimhern <giovannimhern@uchicago.edu>
2024-02-06 22:03:09 -0600

    Added any type to pass run build --> address later

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -8,7 +8,7 @@ import { cookies } from "next/headers";
 
 import { createClient } from "@supabase/supabase-js";
 
-export default async function ProfilePage({ params }) {
+export default async function ProfilePage({ params }: {params: any}) {
   const { id } = params;
   return <h1>This is your profile page...in the works for ID {id ?? "1"}</h1>;
 }
