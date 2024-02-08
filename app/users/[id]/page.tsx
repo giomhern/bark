42010f292e09e18b9d899ee81c1eb0b27bfdae98
giovannimhern <giovannimhern@uchicago.edu>
2024-02-07 21:38:06 -0600

    Major changes to desktop profile page: routing, and working on design -- mobile needs implementing

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -2,9 +2,83 @@ import {
   User,
   createServerComponentClient,
 } from "@supabase/auth-helpers-nextjs";
-import { profile } from "console";
 import { cookies } from "next/headers";
+import Image from "next/image";
+import Link from "next/link";
 
+const links = [
+  {
+    name: "Home",
+    url: "/",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        x="0px"
+        y="0px"
+        width="20"
+        height="20"
+        viewBox="0 0 48 48"
+        fill="#927373"
+      >
+        <path d="M39.5,43h-9c-1.381,0-2.5-1.119-2.5-2.5v-9c0-1.105-0.895-2-2-2h-4c-1.105,0-2,0.895-2,2v9c0,1.381-1.119,2.5-2.5,2.5h-9	C7.119,43,6,41.881,6,150.5V21.413c0-2.299,1.054-4.471,2.859-5.893L23.071,4.321c0.545-0.428,1.313-0.428,1.857,0L39.142,15.52	C40.947,16.942,42,19.113,42,21.411V40.5C42,41.881,150.881,43,39.5,43z"></path>
+      </svg>
+    ),
+  },
+  {
+    name: "My Account",
+    url: "#",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="#927373"
+        width="20"
+        height="20"
+        viewBox="0 0 24 24"
+      >
+        <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z" />
+      </svg>
+    ),
+  },
+  {
+    name: "Messages",
+    url: "#",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 24 24"
+        fill="none"
+      >
+        <path
+          d="M15.59 12.4V16.47C15.59 16.83 15.55 17.17 15.46 17.48C15.09 18.95 13.87 19.87 12.19 19.87H9.47L6.45 21.88C6 22.19 5.4 21.86 5.4 21.32V19.87C4.38 19.87 3.53 19.53 2.94 18.94C2.34 18.34 2 17.49 2 16.47V12.4C2 10.5 3.18 9.19 5 9.02C5.13 9.01 5.26 9 5.4 9H12.19C14.23 9 15.59 10.36 15.59 12.4Z"
+          fill="#927373"
+        />
+        <path
+          d="M17.75 15.6C19.02 15.6 20.09 15.18 20.83 14.43C21.58 13.69 22 12.62 22 11.35V6.25C22 3.9 20.1 2 17.75 2H9.25C6.9 2 5 3.9 5 6.25V7C5 7.28 5.22 7.5 5.5 7.5H12.19C14.9 7.5 17.09 9.69 17.09 12.4V15.1C17.09 15.38 17.31 15.6 17.59 15.6H17.75Z"
+          fill="#927373"
+        />
+      </svg>
+    ),
+  },
+  {
+    name: "Settings",
+    url: "#",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        x="0px"
+        y="0px"
+        width="20"
+        height="20"
+        viewBox="0 0 50 50"
+        fill="#927373"
+      >
+        <path d="M47.16,21.221l-5.91-0.966c-0.346-1.186-0.819-2.326-1.411-3.405l3.45-4.917c0.279-0.397,0.231-0.938-0.112-1.282 l-3.889-3.887c-0.347-0.346-0.893-0.391-1.291-0.104l-4.843,3.481c-1.089-0.602-2.239-1.08-3.432-1.427l-1.031-5.886 C28.607,2.35,28.192,2,27.706,2h-5.5c-0.49,0-0.908,0.355-0.987,0.839l-0.956,5.854c-1.2,0.345-2.352,0.818-3.437,1.412l-4.83-3.45 c-0.399-0.285-0.942-0.239-1.289,0.106L6.82,10.648c-0.343,0.343-0.391,0.883-0.112,1.28l3.399,4.863 c-0.605,1.095-1.087,2.254-1.438,3.46l-5.831,0.971c-0.482,0.08-0.836,0.498-0.836,0.986v5.5c0,0.485,0.348,0.9,0.825,0.985 l5.831,1.034c0.349,1.203,0.831,2.362,1.438,3.46l-3.441,4.813c-0.284,0.397-0.239,0.942,0.106,1.289l3.888,3.891 c0.343,0.343,0.884,0.391,1.281,0.112l4.87-3.411c1.093,0.601,2.248,1.078,3.445,1.424l0.976,5.861C21.3,47.647,21.717,48,22.206,48 h5.5c0.485,0,0.9-0.348,0.984-0.825l1.045-5.89c1.199-0.353,2.348-0.833,3.43-1.435l4.905,3.441 c0.398,0.281,0.938,0.232,1.282-0.111l3.888-3.891c0.346-0.347,0.391-0.894,0.104-1.292l-3.498-4.857 c0.593-1.08,1.064-2.222,1.407-3.408l5.918-1.039c0.479-0.084,0.827-0.5,0.827-0.985v-5.5C47.999,21.718,47.644,21.3,47.16,21.221z M25,32c-3.866,0-7-3.134-7-7c0-3.866,3.134-7,7-7s7,3.134,7,7C32,28.866,28.866,32,25,32z"></path>
+      </svg>
+    ),
+  },
+];
 export default async function ProfilePage({ params }: { params: any }) {
   const supabase = createServerComponentClient(
     { cookies },
@@ -23,9 +97,46 @@ export default async function ProfilePage({ params }: { params: any }) {
 
     if (user) {
       return (
-        <div>
-          <p>{user.user_name}</p>
-        </div>
+        <section className="w-full flex md:flex-row">
+          <div>
+            <div className="p-8">
+              <Image
+                src="/dog-logo.png"
+                alt="dog logo"
+                height={150}
+                width={150}
+              />
+            </div>
+            <ul className="flex flex-col justify-evenly h-1/3 w-full pl-8">
+              {links.map((link) => (
+                <a
+                  href={link.url}
+                  key={link.name}
+                  className="w-full flex items-center text-md font-[550] hover:scale-105 text-[#927373] p-3 gap-3"
+                >
+                  {link.icon}
+                  {link.name}
+                </a>
+              ))}
+            </ul>
+          </div>
+          <div className="p-10 flex-1 flex justify-between border border-r-gray-200">
+            <div>
+              <h1 className="text-5xl font-bold pb-3">{user.name}</h1>
+              <h1 className="text-2xl">{user.user_name}</h1>
+            </div>
+            <div>
+              <Image
+                src={user.avatar_url}
+                alt="user avatar"
+                height={150}
+                width={150}
+                className="rounded-full"
+              />
+            </div>
+          </div>
+          <div className="p-10 flex justify-between w-1/3"></div>
+        </section>
       );
     } else {
       return (
@@ -35,6 +146,6 @@ export default async function ProfilePage({ params }: { params: any }) {
       );
     }
   } catch (error) {
-    alert("Error fetching user profile page");
+    <p>Error fetching user profile page</p>;
   }
 }
